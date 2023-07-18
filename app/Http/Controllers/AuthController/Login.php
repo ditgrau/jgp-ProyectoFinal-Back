919517fb8c08b6386a5f43ec52e1accd12e79605
DitGrau <juditgraup@gmail.com>
2023-07-18 23:44:19 +0200

    feat: funcion login

---
+++ app/Http/Controllers/AuthController/Login.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\AuthController;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class Login extends Controller
+{
+    public function __invoke(Request $request){
+        try {
+            $validator = Validator::make($request->all(),[
+                'email' => 'required|email',
+                'password' => 'required'
+            ], [
+                'email' => 'Email or password are invalid',
+                'password' => 'Email or password are invalid'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $validData = $validator->validated();
+
+            $user = User::where('email', $validData['email'])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'message' => 'Email or password are invalid'
+                ], Response::HTTP_FORBIDDEN);
+            }
+
+            if (!Hash::check($validData['password'], $user->password)) {
+                return response()->json([
+                    'message' => 'Email or password are invalid'
+                ], Response::HTTP_FORBIDDEN);
+            }
+
+            if($user->confirmed){
+                $token = $user->createToken('apiToken')->plainTextToken;
+            }
+
+            return response()->json([
+                'message' => 'User registered',
+                'data' => $user,
+                'token' => $token
+            ], Response::HTTP_CREATED);
+
+        } catch (\Throwable $th) {
+            Log::error('Error logging user in ' . $th->getMessage());
+
+            return response()->json([
+                'message' => 'Error logging user in'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController\Login;
 use App\Http\Controllers\AuthController\Register;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // AUTH CONTROLLER
 Route::post('/register', Register::class);
+Route::post('/login', Login::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
