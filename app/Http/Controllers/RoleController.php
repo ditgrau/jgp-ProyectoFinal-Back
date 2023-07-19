a42b023cf6e77db87e3a736694c21b94fcece97a
DitGrau <juditgraup@gmail.com>
2023-07-19 18:01:29 +0200

    feat: getallroles para registro desde vista admin, y enrutamiento

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RoleController extends Controller
+{
+    public function getAllRoles()
+    {
+        try {
+            $roles = Role::all();
+            
+            return response()->json([
+                'message' => 'Roles retrieved',
+                'data' => $roles,
+                'success' => true
+            ], Response::HTTP_OK);
+
+        } catch (\Throwable $th) {
+            Log::error('Error getting roles' . $th->getMessage());
+
+            return response()->json([
+                'message' => 'Error retrieving roles'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController\Login;
 use App\Http\Controllers\AuthController\Register;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ Route::get('/welcome', function () {
 // AUTH CONTROLLER
 Route::post('/register', Register::class);
 Route::post('/login', Login::class);
+
+// GROUPS CONTROLLER
+Route::get('/getAllGroups', [GroupController::class, 'getAllGroups']);
+
+// ROLES CONTROLLER
+Route::get('/getAllRoles', [RoleController::class, 'getAllRoles']);
+
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
