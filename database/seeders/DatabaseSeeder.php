6e74ecb91d6bcafd36d694073fe116ba88d7ee2d
DitGrau <juditgraup@gmail.com>
2023-07-24 08:50:01 +0200

    "feat: factories para confirmacion de usuario"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        User::factory(10)->create();
+        User::factory(10)->create(new Sequence(['confirmed' => true], ['confirmed' => false]))->create();
         User_data::factory(13)->state(new Sequence(['user_id' => 1], ['user_id' => 2], ['user_id' => 3], ['user_id' => 4], ['user_id' => 5], ['user_id' => 6], ['user_id' => 7], ['user_id' => 8], ['user_id' => 9], ['user_id' => 10], ['user_id' => 11], ['user_id' => 12], ['user_id' => 13]))->create();
         Event::factory(30)->state(new Sequence(['event_type_id' => 1, 'name' => 'Competicion'], ['event_type_id' => 2, 'name' => 'Entrenamiento extra'], ['event_type_id' => 3,'name' => 'Descanso']))->create();
         Result::factory(50)->state(new Sequence(['name' => 'Provincial'], ['name' => 'Autonomico'], ['name' => 'Nacional'], ['name' => 'Jocs'], ['name' => 'Trofeo Atzar'], ['name' => 'Trofeo Ruben Orihuela']))->create();
