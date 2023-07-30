c20f7b24a08b63abc48e032e83fcf7152dd1139e
DitGrau <juditgraup@gmail.com>
2023-07-30 15:00:51 +0200

    "fix: pruebas de deploy, error 500 por clave"

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,6 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->prefix('')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
